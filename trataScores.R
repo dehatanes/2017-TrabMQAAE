e852cd4db70ce2c6aa7495d9b09e41dd57cce410
albuq <barbara.albuquerque@usp.br>
2017-11-09 00:17:39 -0200

    Trata as scores: diferenca e media
    falta inseri-las nas tabelas corretamente

--- trataScores.R
+++ trataScores.R
@@ -1,51 +1,71 @@
 # Separar as pontuações
 # criando um tipo de dado nomeado Score para teste
 # coloco nele a pontuacao do dataset normal
+library(readr)
+mydata <- read_csv("D:/Meus Documentos/USP/atp_matches_2016.csv")
+# rm (scores)
+scores <- na.omit(mydata$score)
+scores <- data.frame(scores, stringsAsFactors = FALSE)
 
-scores <- mydata$score
-scores <- data.frame(scores)
 
-# crio colunas para cada um dos sets
+
+# crio colunas para cada um dos sets e para diferencas e medias
 scores$first_set <- lapply(strsplit(as.character(scores$scores), "\\ "), "[", 1)
+scores$first_diferenca <- NA
+scores$first_media <- NA
 scores$second_set <- lapply(strsplit(as.character(scores$scores), "\\ "), "[", 2)
+scores$second_diferenca <- NA
+scores$second_media <- NA
 scores$third_set <- lapply(strsplit(as.character(scores$scores), "\\ "), "[", 3)
+scores$third_diferenca <- NA
+scores$third_media <- NA
 
-# comeco a calcular a diferenca das pontuacoes por set
-scores$diferenca <- strsplit(as.character(scores$first_set),'-') 
 
-# criou um vetor com dois valores como caractere na coluna
-vetor <- scores$diferenca
 
-# ideia: achar a difrenca entre ambos
-vals <- numeric()
 
-for (i in 1:length(vetor)){
-  if (is.na(vetor[i]))    next 
-  if (('\\(' %in% vetor[i])==TRUE)  print(vetor[i])
-  vals <- append(vals, Reduce("-", vetor[i]))
+# cria funcao para calcular e registrar as diferencas e medias
+calculaSet <- function(set, ordem){
+  resultado <- list("diferenca", "media")
+  for (i in 1:length(set)){
+      # pega a pontuação dos sets
+      primeiro <- as.numeric(substr(set[i], start=1, stop=1))
+      segundo <- as.numeric(substr(set[i], start=3, stop=3))
+      
+      # calcula a diferença
+      diferenca <- primeiro - segundo
+      if (!is.na(diferenca) && diferenca < 1) diferença <- diferenca * (-1)
+      
+      # calcula a media
+      media <- (primeiro+segundo)/2
+      
+      # registra esse resultado no dataframe
+      #resultado <- c(resultado, diferenca, media)
+      switch (ordem,
+        "1" = {
+          scores$first_diferenca[i] <- diferenca
+          scores$first_media <- media
+          # cat("\n", i, " dif: ", diferenca, " media: ", media)
+        },
+        "2" = {
+          scores$second_diferenca[i] <- diferenca
+          scores$second_media[i] <- media
+        },
+        "3" = {
+          scores$third_diferenca[i] <- diferenca
+          scores$third_media[i] <- media
+        }, stop
+      )
+  }
+  return (resultado)
 }
-print(vals)
 
 
+# utiliza a funcao pra o 1 set
+q <- calculaSet(scores$first_set, "1")
 
-resultado <- (sum(scores$diferenca))
-scores$diferenca <- lapply(sum(as.numeric(scores$diferenca)))
-funcao_soma <- function(vetor){
-  operando1 = as.numeric(out)
-}
-out <- unlist(out, use.names=FALSE)
-do.call(sum, out)
+print (q)
+# utiliza a funcao pra o 2 set
+calculaSet(scores$second_set, 2)
+# utiliza a funcao pra o 3 set
+calculaSet(scores$third_set, 3)
 
-soma <- function(lista){
-  sum()
-}
-
-first_number <- strsplit(as.character(scores$first_set), "\\-")
-first_number <- lapply(first_number, sum(x))
-do.call(sum, first_number)
-second_number <- strsplit(as.character(scores$first_set), "\\-")
-
-
-scores <- data.frame(do.call(strsplit(as.character(scores$scores),' ')))
-
-library(stringr)
